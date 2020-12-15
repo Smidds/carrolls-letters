d40143dc8c55fe6344804868fd42e438660ef5df
Isaac Smith <isaachsmith.dev@gmail.com>
2020-12-15 11:46:44 +0700

    feat: get object construction minimally functional

--- scripts/parse-docs.js
+++ scripts/parse-docs.js
@@ -2,36 +2,53 @@ const path = require('path')
 const fs = require('fs')
 const glob = require('glob')
 const consola = require('consola')
+const parseDateString = require('date-fns/parse')
 const HTMLParser = require('node-html-parser')
 
 const LETTERS_INPUT_DIR = 'scripts/assets/letters'
 const LETTERS_OUTPUT_DIR = 'content/letters'
 const SECTION_TYPES = {
-  HEADER: 0,
-  FOOTNOTE: 1,
-  TEXT: 2,
-  IMAGE: 3
+  HEADER: 'header',
+  FOOTNOTE: 'footnote',
+  TEXT: 'text',
+  IMAGE: 'image'
 }
 const HEADER_VARIATIONS = {
-  STANDARD: 0,
-  NOTE: 1
+  STANDARD: 'standard',
+  NOTE: 'note'
 }
 
-const formatAttributes = attributesOBj => Object.entries(attributesOBj).map(([key, value]) => `${key}="${value}"`).join(' ')
+const getDate = (headerStr) => {
+  const dateSearch = /^\d+-\d+-\d+/.exec(headerStr)
 
-const isEmpty = (node, previousNode) => previousNode.type === SECTION_TYPES.HEADER ? !!node.text.replace(/^[\s-–]*/, '') : !!node.text.replace(/^[\s]*/, '')
+  if (dateSearch) return parseDateString(dateSearch[0], 'd-M-yy', new Date())
+  return headerStr.replace(/^no\s*date[\s-–]*/gi, '')
+}
 
-const isBold = node => /<b>/ig.test(node.toString())
+const isEmpty = (node, previousNode) => previousNode && previousNode.type === SECTION_TYPES.HEADER ? !node.text.replace(/^[\s-–]*/, '') : !node.text.replace(/^[\s]*/, '')
 
-const isUnderlined = node => /<u>/ig.test(node.toString())
-
-const isLetterHead = node => /^\d+-\d+-\d+|^no date/i.test(node.text.trim())
+const isLetterHead = node => /^\d+-\d+-\d+|^\d+-\?-\d+|^no\s*date/i.test(node.text.trim())
 
 const isBlue = node => /color="#0070c0"/i.test(node.toString())
 
 const isImage = node => /<img/i.test(node.toString())
 
 const isCentered = node => /align="CENTER"/i.test(node.toString())
+
+const formatText = (node) => {
+  if (node.childNodes.length === 0) return node.text.replace(/\n/g, ' ')
+
+  const formatChildNodes = () => node.childNodes.map(child => formatText(child)).join('')
+
+  if (/b/i.test(node.tagName)) {
+    return `<b>${formatChildNodes()}</b>`
+  } else if (/u/i.test(node.tagName)) {
+    return `<u>${formatChildNodes()}</u>`
+  } else if (/wingdings/i.test(node.getAttribute('face'))) {
+    return `<span class="wingdings">${formatChildNodes()}</span>`
+  }
+  return formatChildNodes()
+}
 
 const createLetters = (paragraphs) => {
   const letters = []
@@ -42,9 +59,8 @@ const createLetters = (paragraphs) => {
       letters.push(currentLetter)
     }
 
-    const dateSearch = /^\d+-\d+-\d+/.exec(node.text)
     currentLetter = {
-      date: dateSearch ? dateSearch[0] : null,
+      date: getDate(node.text),
       variation: isBlue(node) ? HEADER_VARIATIONS.NOTE : HEADER_VARIATIONS.STANDARD,
       sections: [
         { type: SECTION_TYPES.HEADER }
@@ -52,7 +68,7 @@ const createLetters = (paragraphs) => {
     }
   }
 
-  const getPreviousSection = () => currentLetter.sections[currentLetter.sections.length - 1]
+  const getPreviousSection = () => currentLetter ? currentLetter.sections[currentLetter.sections.length - 1] : {}
 
   paragraphs.forEach((paragraph) => {
     if (isCentered(paragraph)) {
@@ -61,35 +77,38 @@ const createLetters = (paragraphs) => {
           type: SECTION_TYPES.IMAGE,
           sources: [...paragraph.toString().matchAll(/src="(.*?)"/gi)].map(([, res]) => res)
         })
+      } else {
+        const prevSection = getPreviousSection()
+
+        paragraph.childNodes.forEach((node) => {
+          if (!isEmpty(node, prevSection)) {
+            prevSection.caption
+              ? prevSection.caption += formatText(node)
+              : prevSection.caption = formatText(node)
+          }
+        })
+
+        prevSection.caption += '\n\n'
       }
     } else {
       paragraph.childNodes.forEach((node) => {
-        if (!isEmpty(node)) {
+        if (!isEmpty(node, getPreviousSection())) {
           if (isLetterHead(node)) {
             startNewLetter(node)
-          } else if (isBlue(node) && getPreviousSection().variation !== HEADER_VARIATIONS.NOTE) {
+          } else if (isBlue(node) && currentLetter.variation !== HEADER_VARIATIONS.NOTE) {
             if (getPreviousSection().type === SECTION_TYPES.HEADER) {
-              getPreviousSection().footnote = node
-            }
-          } else if (isBold(node)) {
-            if (getPreviousSection().type === SECTION_TYPES.TEXT) {
-              getPreviousSection().value += `<b>${node.text}</b>`
-            } else if (getPreviousSection().type !== SECTION_TYPES.HEADER) {
-              currentLetter.sections.push({
-                type: SECTION_TYPES.TEXT,
-                value: `<ul>${node.text}</ul>`
-              })
-            }
-          } else if (isUnderlined(node)) {
-            if (getPreviousSection().type === SECTION_TYPES.TEXT) {
-              getPreviousSection().value += `<ul>${node.text}</ul>`
+              getPreviousSection().footnote = formatText(node)
             } else {
               currentLetter.sections.push({
-                type: SECTION_TYPES.TEXT,
-                value: `<ul>${node.text}</ul>`
+                type: SECTION_TYPES.FOOTNOTE,
+                content: formatText(node)
               })
             }
-          }
+          } else if (getPreviousSection().type === SECTION_TYPES.TEXT) getPreviousSection().content += formatText(node)
+          else currentLetter.sections.push({
+            type: SECTION_TYPES.TEXT,
+            content: formatText(node)
+          })
         }
       })
     }
@@ -99,41 +118,12 @@ const createLetters = (paragraphs) => {
   return letters
 }
 
-const constructYearIntroduction = (element) => {
-  const children = element.childNodes
-  const extraAttributes = {}
-  if (element.getAttribute && element.getAttribute('face') && element.getAttribute('face').toLowerCase().includes('wingdings')) {
-    if (extraAttributes.class) extraAttributes.class = `wingdings ${extraAttributes.class}`
-    else extraAttributes.class = 'wingdings'
-  }
-
-  if (children.length === 0) {
-    return element.text.replace(/\n\s*/gi, ' ')
-  }
-
-  if (element.tagName === 'i') {
-    return `<i ${formatAttributes(extraAttributes)}>${element.childNodes.map(node => constructYearIntroduction(node)).join('')}</i>`
-  }
-
-  if (element.tagName === 'b') {
-    return `<b ${formatAttributes(extraAttributes)}>${element.childNodes.map(node => constructYearIntroduction(node)).join('')}</b>`
-  }
-
-  if (element.tagName === 'p') {
-    return `<p ${formatAttributes(extraAttributes)}>${element.childNodes.map(node => constructYearIntroduction(node)).join('')}</p>`
-  }
-
-  return Object.keys(extraAttributes).length > 0
-    ? `<span ${formatAttributes(extraAttributes)}>${element.childNodes.map(node => constructYearIntroduction(node)).join('')}</span>`
-    : element.childNodes.map(node => constructYearIntroduction(node)).join('')
-}
-
 const parseYearDocument = (docPath) => {
   const document = fs.readFileSync(docPath)
   const year = /(\d+)/.exec(path.basename(docPath))[1]
   const root = HTMLParser.parse(document).querySelector('body')
   const paragraphs = root.querySelectorAll('p')
-  const intro = constructYearIntroduction(paragraphs[1]).trim()
+  const intro = formatText(paragraphs[1]).trim()
 
   return {
     year,
@@ -152,12 +142,16 @@ const generateYearsLetters = () => {
     return
   }
 
-  const formattedYears = lettersByYearDocuments.map(docPath => parseYearDocument(docPath))
+  try {
+    const formattedYears = lettersByYearDocuments.map(docPath => parseYearDocument(docPath))
 
-  consola.info(`Writing formatted markdown documents to ./${LETTERS_OUTPUT_DIR}`)
-  formattedYears.forEach((doc) => {
-    fs.writeFileSync(path.resolve(`${LETTERS_OUTPUT_DIR}/${doc.year}.md`), doc.body)
-  })
+    consola.info(`Writing formatted markdown documents to ./${LETTERS_OUTPUT_DIR}`)
+    formattedYears.forEach((doc) => {
+      fs.writeFileSync(path.resolve(`${LETTERS_OUTPUT_DIR}/${doc.year}.md`), `# ${doc.year}\n\n${doc.intro}\n\n${JSON.stringify(doc.letters)}`)
+    })
+  } catch (err) {
+    consola.error(err)
+  }
 }
 
 generateYearsLetters()
