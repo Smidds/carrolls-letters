a5182b60a2c3b9c59fd71f539d927334661b652c
Isaac Smith <isaachsmith.dev@gmail.com>
2020-12-16 18:39:46 +0700

    feat: make images clickable

--- components/global/LetterImage.vue
+++ components/global/LetterImage.vue
@@ -8,9 +8,10 @@
         max-height="300"
         max-width="500"
         eager
-        class="mb-3"
+        class="mb-3 clickable-image"
         :src="`${baseUrl}/images/${source}`"
         :alt="captionText"
+        @click="open(`${baseUrl}/images/${source}`)"
       />
     </div>
     <div v-if="$slots.default" class="caption primary--text mx-auto px-2 px-md-5">
@@ -38,12 +39,21 @@ export default {
     captionText () {
       return this.$slots.default ? domParser(this.$slots.default).text : ''
     }
+  },
+  methods: {
+    open (imagePath) {
+      window.open(imagePath, '_blank')
+    }
   }
 }
 </script>
 
 <style lang="scss">
 .letter-image {
+  .clickable-image {
+    cursor: pointer;
+  }
+
   .v-image {
     margin-left: auto;
     margin-right: auto;
